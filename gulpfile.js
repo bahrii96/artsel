ddf82af7d42c8a60b9ca066c6036a0ef5b9bcc23
DESKTOP-VLTDHS8\bahri <bahriivadim@gmail.com>
2023-03-21 11:58:48 +0200

    add progect

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,69 @@
-let	gulp         = require('gulp'),
-	rename 		 = require('gulp-rename'),
-	rigger 		 = require('gulp-rigger'),
-	sass         = require('gulp-sass'),
-	browserSync  = require('browser-sync'),
-	cleancss     = require('gulp-clean-css'),
-	autoprefixer = require('gulp-autoprefixer');
+// Импорт основного модуля
+import gulp from "gulp";
+// Импорт общих плагинов
+import { plugins } from "./config/gulp-plugins.js";
+// Импорт путей
+import { path } from "./config/gulp-settings.js";
 
-gulp.task('browser-sync', function() {
-	browserSync({
-		server: {baseDir: 'app'},
-		notify: false,
-	})
-});
+// Передаем значения в глобальную переменную
+global.app = {
+	isBuild: process.argv.includes('--build'),
+	isDev: !process.argv.includes('--build'),
+	isWebP: !process.argv.includes('--nowebp'),
+	isFontsReW: process.argv.includes('--rewrite'),
+	gulp: gulp,
+	path: path,
+	plugins: plugins
+}
 
-function bsReload(done) { browserSync.reload(); done(); };
+// Импорт задач
+import { reset } from "./config/gulp-tasks/reset.js";
+import { html } from "./config/gulp-tasks/html.js";
+import { css } from "./config/gulp-tasks/css.js";
+import { js } from "./config/gulp-tasks/js.js";
+import { jsDev } from "./config/gulp-tasks/js-dev.js";
+import { images } from "./config/gulp-tasks/images.js";
+import { ftp } from "./config/gulp-tasks/ftp.js";
+import { zip } from "./config/gulp-tasks/zip.js";
+import { sprite } from "./config/gulp-tasks/sprite.js";
+import { gitignore } from "./config/gulp-tasks/gitignore.js";
+import { otfToTtf, ttfToWoff, fonstStyle } from "./config/gulp-tasks/fonts.js";
 
-gulp.task('styles', function() {
-	return gulp.src('app/scss/**/*.scss')
-	.pipe(sass({ outputStyle: 'expanded' }))
-	.pipe(autoprefixer({
-		grid: true,
-		overrideBrowserslist: ['last 10 versions']
-	}))
-	.pipe(gulp.dest('app/css'))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } } }))
-	.pipe(rename('main.min.css'))
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.stream())
-});
+// Последовательная обработака шрифтов
+const fonts = gulp.series(reset, otfToTtf, ttfToWoff, fonstStyle);
+// Основные задачи будем выполнять параллельно после обработки шрифтов
+const devTasks = gulp.parallel(fonts, gitignore);
+// Основные задачи будем выполнять параллельно после обработки шрифтов
+const buildTasks = gulp.series(fonts, jsDev, js, gulp.parallel(html, css, images, gitignore));
 
-gulp.task('html', function() {
-	return gulp.src('app/html-dev/*.html')
-	.pipe(rigger())
-	.pipe(gulp.dest('app/'))
-	.pipe(browserSync.reload({ stream: true }))
-});
+// Экспорт задач
+export { html }
+export { css }
+export { js }
+export { jsDev }
+export { images }
+export { fonts }
+export { sprite }
+export { ftp }
+export { zip }
 
-gulp.task('scripts', function() {
-	return gulp.src('app/js/**/*.js')
-	.pipe(browserSync.reload({ stream: true }))
-});
+// Построение сценариев выполнения задач
+const development = gulp.series(devTasks);
+const build = gulp.series(buildTasks);
+const deployFTP = gulp.series(buildTasks, ftp);
+const deployZIP = gulp.series(buildTasks, zip);
 
-gulp.task('watch', function() {
-	gulp.watch('app/scss/**/*.scss', gulp.parallel('styles'));
-	gulp.watch('app/js/**/*.js', gulp.parallel('scripts'));
-	gulp.watch('app/html-dev/**/*.html', gulp.parallel('html'));
-});
+// Экспорт сценариев
+export { development }
+export { build }
+export { deployFTP }
+export { deployZIP }
 
-gulp.task('default', gulp.parallel('styles', 'browser-sync', 'watch'));
+// Выполнение сценария по умолчанию
+gulp.task('default', development);
+
+
+
+
+
+
